e130021bb117fb830aea5d82afb4b40acebc1e93
mohammad fazel <mdhesari99@gmail.com>
2019-06-28 03:00:30 +0430

    implement cookies

---
+++ cookies.php
@@ -0,0 +1,9 @@
+<?php
+
+// 86400s => 1 day
+if (!isset($_COOKIE['test1'])) {
+    setcookie('test1', 'mohamad', time() + 86400 * 30, '/');
+    echo 'Hello, You are all set please agree cookie privacy policy.';
+} else {
+    echo "Hello " . $_COOKIE['test1'] . ' you are an old user.';
+}
