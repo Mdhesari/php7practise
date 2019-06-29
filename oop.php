bbdd9ca2313eb0cc85c40b95b391bc6c08f7e944
mohammad fazel <mdhesari99@gmail.com>
2019-06-29 19:28:08 +0430

    pass by reference variables &

--- oop.php
+++ oop.php
@@ -1,2 +1,26 @@
 <?php
 
+class Test
+{
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+}
+
+$mytest = new Test('ahmad');
+
+$test1 = 1;
+$test2 = &$test1;
+++$test2;
+echo $test1;
+echo "<br>";
+echo $test2;
