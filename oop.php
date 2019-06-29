68417705d70a557a26d1d737f5eb4d825a263b17
mohammad fazel <mdhesari99@gmail.com>
2019-06-30 03:33:05 +0430

    using anonymous classes and trying out oop challenge

--- oop.php
+++ oop.php
@@ -14,13 +14,27 @@ class Test
         return $this->name;
     }
 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 }
 
 $mytest = new Test('ahmad');
+echo $mytest->getName() . '<br>';
+$mytest2 = clone $mytest;
+$mytest2->setName('reza');
+echo 'test 2' . $mytest2->getName() . '<br>';
+echo 'test 1 ' . $mytest->getName() . '<br>';
 
-$test1 = 1;
-$test2 = &$test1;
-++$test2;
-echo $test1;
-echo "<br>";
-echo $test2;
+// $test1 = 1;
+// $test2 = &$test1;
+// ++$test2;
+// echo $test1;
+// echo "<br>";
+// echo $test2;
