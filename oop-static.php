9943c0ab638b54eab626bee86d73470c69eb905b
mohammad fazel <mdhesari99@gmail.com>
2019-06-30 03:45:21 +0430

    some practice on static method (simple)

--- oop-static.php
+++ oop-static.php
@@ -1,2 +1,21 @@
 <?php
 
+class hyperClass
+{
+    public static $name = "MyName";
+    public static function setName($name)
+    {
+        self::$name = $name;
+    }
+}
+
+class superClass extends hyperClass
+{ 
+    public static function setName($name)
+    {
+        parent::$name = $name . " -- ";
+    }
+}
+
+superClass::setName('mohamad');
+echo superClass::$name;
