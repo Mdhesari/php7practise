f054b6fb16672d8912dde2987f7756c8b1a31d0c
mohammad fazel <mdhesari99@gmail.com>
2019-06-30 18:20:50 +0430

    practice abstract class and challenge with split operator ...

--- oop-abstract.php
+++ oop-abstract.php
@@ -1,5 +1,5 @@
 <?php
-
+// A foundation for another class, some methods are already defined and must be used
 abstract class Template
 {
     protected function addSugar()
@@ -52,3 +52,73 @@ class Coffee extends Template
 
 $myCoffee = new Tea;
 $myCoffee->make();
+
+abstract class Food
+{
+    protected $name, $colour;
+
+    public function __construct($name, $colour)
+    {
+        $this->name = $name;
+        $this->colour = $colour;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getColour()
+    {
+        return $this->colour;
+    }
+
+    public abstract function getData();
+}
+
+class Benana extends Food
+{
+
+    protected $price;
+
+    public function __construct($name, $price, $colour = null)
+    {
+        // parent::__construct(...func_get_args());
+        parent::__construct($name, $colour);
+        $this->price = $price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getData()
+    {
+        echo "Hello and Welcome.<br>";
+        echo "Name : " . $this->name . "<br>";
+        echo "Price : " . $this->price . "<br>";
+    }
+}
+
+$mybenana = new Benana('benana', 10000);
+$mybenana->getData();
+
+$arr = array(1, 3, 4);
+
+function plusNumbers(...$arr)
+{
+    $sum = 0;
+    foreach ($arr as $num) {
+        $sum += $num;
+    }
+    return $sum;
+}
+
+function add($n1, $n2, $n3)
+{
+    return $n1 + $n2 + $n3;
+}
+
+// echo add(...$arr);
+echo plusNumbers(1,3,4,32,5);
